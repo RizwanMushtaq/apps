45c7ace680b26d6c9d1cf159b59589cfc431c15e
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2026-04-20 10:30:22 +0200

    [api] added nestjs/config

--- full-stack-turbo-app/apps/api/src/app.module.ts
+++ full-stack-turbo-app/apps/api/src/app.module.ts
@@ -5,22 +5,28 @@ import { join } from 'path';
 import { TodosModule } from './todos/todos.module';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
             autoSchemaFile: join(process.cwd(), 'schema.gql'),
+            playground: process.env.NODE_ENV !== 'production',
         }),
         TodosModule,
         CoffeesModule,
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'postgres',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT ?? '5432', 10),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
             autoLoadEntities: true,
             synchronize: false,
         }),
