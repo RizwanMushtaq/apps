bf80b1c5023071e2a65f6c029d13d1abe5d5d292
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2026-05-02 21:57:47 +0200

    feat: [nest-advance-concepts] used ConfigService in main.ts and ValidationPipe

--- nest-advance-concepts/src/main.ts
+++ nest-advance-concepts/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { APP_CONFIG_KEY, AppConfig } from './config/app.config';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+    app.enableCors({ origin: 'http://localhost:5173' });
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+    const config = app.get(ConfigService);
+    const port = config.get<AppConfig>(APP_CONFIG_KEY)?.port ?? 3000;
+
+    await app.listen(port);
 }
 void bootstrap();
