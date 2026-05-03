d444ee4bdbd4e1e61820f8ba037ecf28cf0aeb9d
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2026-05-03 16:22:54 +0200

    feat: [nest-advance-concepts] added mock auth guard

--- nest-advance-concepts/src/users/users.controller.ts
+++ nest-advance-concepts/src/users/users.controller.ts
@@ -7,16 +7,19 @@ import {
     ParseIntPipe,
     Patch,
     Post,
+    UseGuards,
 } from '@nestjs/common';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/updateUser.dto';
+import { MockAuthGuard } from '../common/guards/mock-auth.guard';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post()
+    @UseGuards(MockAuthGuard)
     async create(@Body() createUserDto: UserDto) {
         return this.usersService.create(createUserDto);
     }
@@ -32,6 +35,7 @@ export class UsersController {
     }
 
     @Patch(':id')
+    @UseGuards(MockAuthGuard)
     async update(
         @Param('id', ParseIntPipe) id: number,
         @Body() updateUserDto: UpdateUserDto,
