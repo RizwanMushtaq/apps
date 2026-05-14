d72f3d6c236d80b2a7ae3029a7a1e5cc0c4c1d1f
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2026-05-14 11:04:32 +0200

    feat: [nest-advance-concepts] added swagger docs

--- nest-advance-concepts/src/main.ts
+++ nest-advance-concepts/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { APP_CONFIG_KEY, AppConfig } from './config/app.config';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
         }),
     );
     const config = app.get(ConfigService);
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Nest Advance Concepts API')
+        .setDescription('The Nest Advance Concepts API description')
+        .setVersion('0.1')
+        .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
+
     const port = config.get<AppConfig>(APP_CONFIG_KEY)?.port ?? 3000;
 
     await app.listen(port);
--- nest-advance-concepts/src/users/dtos/updateUser.dto.ts
+++ nest-advance-concepts/src/users/dtos/updateUser.dto.ts
@@ -1,4 +1,4 @@
+import { PartialType } from '@nestjs/swagger';
 import { UserDto } from './user.dto';
-import { PartialType } from '@nestjs/mapped-types';
 
 export class UpdateUserDto extends PartialType(UserDto) {}
