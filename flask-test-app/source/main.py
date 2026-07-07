a7f601b9df7cb988c6aa6165a0bcb9815b1fa37f
Rizwan <rizwanmushtaq15@gmail.com>
2026-07-07 12:59:07 +0200

    feat: updated flask-test-app

--- flask-test-app/source/main.py
+++ flask-test-app/source/main.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-my_app = Flask("My first Flask App")
+my_app = Flask(__name__)
 
 
 @my_app.route("/")
 def hello_world():
-    return "Hello World!"
+    return f"Hello World! from {__name__}"
 
 
 if __name__ == "__main__":
