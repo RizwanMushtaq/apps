b5beb6f1d9e36c47738a2ce836a2b6ba86bd74b7
Rizwan <rizwanmushtaq15@gmail.com>
2026-07-08 08:27:20 +0200

    feat: updated flask-test-app

--- flask-test-app/app.py
+++ flask-test-app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 my_app = Flask(__name__)
 
 
 @my_app.route("/")
 def hello_world():
+    print(request.headers)
+    print(request.args)
     return f"Hello World! from {__name__}"
 
 
